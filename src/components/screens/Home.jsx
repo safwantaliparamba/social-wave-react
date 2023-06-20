fbad223456bd4c78183eb47d3501dd8ef53feaf3
safwantaliparamba <safwanp2420@gmail.com>
2023-06-20 12:39:45 +0530

    CreatePost home section added

--- src/components/screens/Home.jsx
+++ src/components/screens/Home.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 import styled from 'styled-components'
+// import { useSelector } from 'react-redux'
+
+import CreatePost from '../includes/home/posts/CreatePost'
 
 
 const Home = () => {
+    // const { theme } = useSelector(state => state.ui)
+
     return (
         <>
             <Helmet>
                 <title>Home | SocialWaves</title>
             </Helmet>
             <Wrapper>
-                <h1>Home</h1>
+                <CreatePost />
             </Wrapper>
         </>
     )
@@ -19,14 +24,5 @@ const Home = () => {
 export default Home
 
 const Wrapper = styled.section`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    min-height: 100vh;
 
-    h1{
-        font-size: 28px;
-        color: #fff;
-    }
 `
