f00a1107d073a49bbb801cc9a141f95f66a704f8
safwantaliparamba <safwanp2420@gmail.com>
2023-06-25 19:22:37 +0530

    changes

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,43 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { getItem, setItem } from "../components/functions";
 
 
-export const getItem = (title = "", json = false) => {
-    const item = localStorage.getItem(title)
-
-    if (item?.toLowerCase() === "true") {
-        return true;
-    } else if (item?.toLowerCase() === "false") {
-        return false;
-    } else if (item === "null") {
-        return null;
-    } else if (item === "undefined") {
-        return undefined;
-    } else if (json) {
-        return JSON.parse(item)
-    }
-
-    return item
-}
-
-export const setItem = (title, item) => {
-    localStorage.setItem(title, item);
-
-    return true
-}
+// const initialState = {
+//     email: getItem("email"),
+//     name: getItem("name"),
+//     username: getItem("username"),
+//     sessionId: getItem("sessionId"),
+//     accessToken: getItem("accessToken"),
+//     isProMember: getItem("isProMember") ?? false,
+//     refreshToken: getItem("refreshToken"),
+//     isAuthenticated: getItem("isAuthenticated") ?? false,
+//     notificationCount: getItem("notificationCount") ?? 0,
+//     bookmarkCount: getItem("bookmarkCount") ?? 0,
+// }
 
 const initialState = {
-    email: getItem("email"),
-    name: getItem("name"),
-    username: getItem("username"),
-    sessionId: getItem("sessionId"),
-    accessToken: getItem("accessToken"),
-    isProMember: getItem("isProMember") ?? false,
-    refreshToken: getItem("refreshToken"),
+    sessions: getItem("sessions") ?? [],
+    activeIndex: getItem("activeIndex") ?? -1,
     isAuthenticated: getItem("isAuthenticated") ?? false,
-    notificationCount: getItem("notificationCount") ?? 0,
-    bookmarkCount: getItem("bookmarkCount") ?? 0,
 }
 
+console.log(initialState);
 
 const authSlice = createSlice({
     name: "auth",
@@ -54,38 +38,57 @@ const authSlice = createSlice({
                 bookmarkCount
             } = payload
 
-            setItem("name", name)
-            setItem("email", email)
-            setItem("username", username)
-            setItem("sessionId", sessionId)
-            setItem("isAuthenticated", true)
-            setItem("accessToken", accessToken)
-            setItem("refreshToken", refreshToken)
-            setItem("bookmarkCount", bookmarkCount)
-            setItem("notificationCount", notificationCount)
+            // setItem("name", name)
+            // setItem("email", email)
+            // setItem("username", username)
+            // setItem("sessionId", sessionId)
+            // setItem("isAuthenticated", true)
+            // setItem("accessToken", accessToken)
+            // setItem("refreshToken", refreshToken)
+            // setItem("bookmarkCount", bookmarkCount)
+            // setItem("notificationCount", notificationCount)
 
-            return {
-                ...state, ...payload, isAuthenticated: true
+            const isExists = state.sessions.find(session => session.username === username) ?? false
+
+            if (isExists) {
+                let tempSessions = [...state.sessions]
+
+                const index = tempSessions.findIndex(session => session.username === username)
+
+                if (index !== -1) {
+                    tempSessions.splice(index, 0, { ...payload })
+
+                    const tempState = { ...state, sessions: tempSessions, activeIndex: index}
+
+                    return tempState
+                } else {
+                    console.log("user not found with the username " + username);
+                }
+            } else {
+                const tempState = { ...state, isAuthenticated: true }
+                tempState.sessions.push({ ...payload })
             }
+
+
         },
         editUserData: (state, { payload }) => {
-            const {
-                name = state.name,
-                email = state.email,
-                username = state.username,
-                sessionId = state.sessionId,
-                isProMember = state.isProMember,
-                bookmarkCount = state.bookmarkCount,
-                notificationCount = state.notificationCount,
-            } = payload
+            // const {
+            //     name = state.name,
+            //     email = state.email,
+            //     username = state.username,
+            //     sessionId = state.sessions,
+            //     isProMember = state.isProMember,
+            //     bookmarkCount = state.bookmarkCount,
+            //     notificationCount = state.notificationCount,
+            // } = payload
 
-            setItem("name", name)
-            setItem("email", email)
-            setItem("username", username)
-            setItem("sessionId", sessionId)
-            setItem("isProMember", isProMember)
-            setItem("bookmarkCount", bookmarkCount)
-            setItem("notificationCount", notificationCount)
+            // setItem("name", name)
+            // setItem("email", email)
+            // setItem("username", username)
+            // setItem("sessionId", sessionId)
+            // setItem("isProMember", isProMember)
+            // setItem("bookmarkCount", bookmarkCount)
+            // setItem("notificationCount", notificationCount)
 
             return { ...state, ...payload }
         },
